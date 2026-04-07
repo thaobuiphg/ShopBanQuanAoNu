57fcc8127fa0f11408db99fd68bc58673e3816cc
thaobuiphg <bthao2023600374@gmail.com>
2026-04-07 18:11:51 +0700

    convert to mongo full

---
+++ admin/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log("❌ MongoDB error:", err));
+
+module.exports = mongoose;
